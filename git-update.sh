4b678e1992133391f9a9dc6d3dc2d3bf6a65eab8
Stephen Poole <stephen.poole@redlioncanada.com>
2015-07-13 14:11:05 -0400

    Merge branch 'master' of https://github.com/rlstephenpoole/cuat
    Conflicts:
    	cuat.whirlpool.ca/en_CA/-3/-[UXB0600DYS]-1301317/UXB0600DYS/index.html
    	cuat.whirlpool.ca/en_CA/-3/-[UXB1200DYS]-1301318/UXB1200DYS/index.html
    	cuat.whirlpool.ca/en_CA/-3/-[UXI1200DYS]-1301324/UXI1200DYS/index.html
    	cuat.whirlpool.ca/en_CA/-[UXB0600DYS]-1301317/UXB0600DYS/index.html
    	cuat.whirlpool.ca/en_CA/-[UXB1200DYS]-1301318/UXB1200DYS/index.html
    	cuat.whirlpool.ca/en_CA/-[WRV976FDEM]-32004/WRV976FDEM/index.html
    	cuat.whirlpool.ca/en_CA/-[WRV996FDEE]-32005/WRV996FDEE/index.html
    	cuat.whirlpool.ca/en_CA/-[YWFE715H0EE]-32012/YWFE715H0EE/index.html
    	cuat.whirlpool.ca/en_CA/Kitchen-1/cooking-2/ranges-3/-[YWFE715H0ES]-32012/YWFE715H0ES/index.html
    	cuat.whirlpool.ca/en_CA/Kitchen-1/refrigeration-2/refrigerators-3/-[WRV976FDEM]-32004/WRV976FDEM/index.html
    	cuat.whirlpool.ca/en_CA/Kitchen-1/refrigeration-2/refrigerators-3/-[WRV996FDEM]-32005/WRV996FDEM/index.html
    	cuat.whirlpool.ca/en_CA/Laundry-1/laundry-2/High--Efficiency-Front-Load-3/102280056/index.html
    	cuat.whirlpool.ca/en_CA/Laundry-1/laundry-2/laundry-pairs-3/-[WFC7500VW]-1301714/WFC7500VW/index.html
    	cuat.whirlpool.ca/en_CA/Laundry-1/laundry-2/laundry-pairs-3/-[YWED7500VW]-1302156/YWED7500VW/index.html
    	git-update.sh

--- git-update.sh
+++ git-update.sh
@@ -2,7 +2,7 @@
 #
 # git update - keep records of all css changes on whirlpool, kitchenaid, and maytag sites
 #
-<<<<<<< HEAD
+
 export PATH=/usr/local/bin:$PATH
 LOCALUSER="stepoole"
 GITUSER="rlstephenpoole"
@@ -11,18 +11,13 @@ AGENT="Mozilla/5.0 (iPhone; CPU iPhone OS 8_0 like Mac OS X) AppleWebKit/600.1.3
 
 mkdir /Users/$LOCALUSER/Documents/cuat
 cd /Users/$LOCALUSER/Documents/cuat
-=======
-
-export PATH=/usr/local/bin:$PATH
-cd /Users/stepoole/Documents/cuat
->>>>>>> 0c23380801de07d6e96b5869add02a21b1aace0e
 wget --recursive -r --page-requisites -A .css,.js,.html --no-parent http://cuat.whirlpool.ca/en_CA/
 wget --recursive -r --page-requisites -A .css,.js,.html --no-parent http://cuat.whirlpool.ca/fr_CA/
 wget --recursive -r --page-requisites -A .css,.js,.html --no-parent http://cuat.maytag.ca/en_CA/
 wget --recursive -r --page-requisites -A .css,.js,.html --no-parent http://cuat.maytag.ca/fr_CA/
 wget --recursive -r --page-requisites -A .css,.js,.html --no-parent http://cuat.kitchenaid.ca/en_CA/
 wget --recursive -r --page-requisites -A .css,.js,.html --no-parent http://cuat.kitchenaid.ca/fr_CA/
-<<<<<<< HEAD
+
 mkdir /Users/$LOCALUSER/Documents/cuat/mobile
 cd /Users/$LOCALUSER/Documents/cuat/mobile
 wget --user-agent $AGENT --recursive -r --page-requisites -A .css,.js,.html --no-parent http://cuat.whirlpool.ca/en_CA/
@@ -31,11 +26,7 @@ wget --user-agent $AGENT --recursive -r --page-requisites -A .css,.js,.html --no
 wget --user-agent $AGENT --recursive -r --page-requisites -A .css,.js,.html --no-parent http://cuat.maytag.ca/fr_CA/
 wget --user-agent $AGENT --recursive -r --page-requisites -A .css,.js,.html --no-parent http://cuat.kitchenaid.ca/en_CA/
 wget --user-agent $AGENT --recursive -r --page-requisites -A .css,.js,.html --no-parent http://cuat.kitchenaid.ca/fr_CA/
+
 git add -A .
 git commit -m "Update"
 git push https://$GITUSER:$GITPASS@github.com/$GITUSER/cuat.git master
-=======
-git add -A .
-git commit -m "Update"
-git push https://rlstephenpoole:Pgr0upe12@github.com/rlstephenpoole/cuat.git master
->>>>>>> 0c23380801de07d6e96b5869add02a21b1aace0e
