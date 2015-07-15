8122ef99bd78dd7d7331f44688fcb690177f5c1f
Stephen Poole <stephen.poole@redlioncanada.com>
2015-07-15 15:12:37 -0400

    Update

--- git-update.sh
+++ git-update.sh
@@ -6,7 +6,7 @@
 export PATH=/usr/local/bin:$PATH
 LOCALUSER="stepoole"
 GITUSER="rlstephenpoole"
-GITPASS="Pgr0upe12"
+GITPASS="foo"
 AGENT="Mozilla/5.0 (iPhone; CPU iPhone OS 8_0 like Mac OS X) AppleWebKit/600.1.3 (KHTML, like Gecko) Version/8.0 Mobile/12A4345d Safari/600.1.4"
 
 mkdir /Users/$LOCALUSER/Documents/cuat
